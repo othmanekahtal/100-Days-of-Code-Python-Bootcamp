1215b15a1692f6bd5439173f1f18f883abcf6846
othmanekahtal <othmanekahtal@gmail.com>
2021-01-27 20:57:49 +0100

    add files for day

---
+++ Day 4/Heads or Tails.py
@@ -0,0 +1,8 @@
+import random
+test_seed=int(input("Create a seed number : "))
+random.seed(test_seed)
+random_side=random.randint(0,1)
+if random_side == 1:
+    print("Head")
+else:
+    print("Tail")
---
+++ Day 4/random module examples.py
@@ -0,0 +1,8 @@
+import random as rdm
+
+# random int between 1-10:
+print(rdm.randint(1,10))
+#random float between 1-5:
+print(str(4*rdm.random()+1))
+# random float between 1-10 :
+print(str(rdm.uniform(1,10)))
